75e2f34ad3dba6986251cce619e9e27c22233363
Tiimgll <timkalevin0507@gmail.com>
2024-04-29 21:27:44 +0300

    start

---
+++ gitlab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
+sudo apt-get install -y postfix
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+sudo EXTERNAL_URL="https://gitlab.tiimgll.home" apt-get install gitlab-ce
+
+cat /etc/gitlab/initial_root_password
